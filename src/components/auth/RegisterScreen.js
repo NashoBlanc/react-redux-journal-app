b4b80e3218282c4b450556f497a55e490ac1d225
ignacioblanc <iiblanc@falabella.cl>
2021-06-09 16:43:50 -0400

    added new reducer and types and useDispatch

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useForm } from '../../hooks/useForm';
 import validator from 'validator';
+import { useDispatch } from "react-redux";
+import { setError, removeError } from '../../actions/ui';
 export const RegisterScreen = () => {
+
+    const dispatch = useDispatch();
 
 
     const [formValues, handleInputChange] = useForm({
@@ -22,15 +26,17 @@ export const RegisterScreen = () => {
     const isFormValid = (e) => {
 
         if(name.trim().length === 0) {
-            console.log('name is required');
-            return false
-        } else if (!validator.isEmail(email)) {
-            console.log('Email is not valid');
-            return false
-        } else if (password !== password2 || password.length < 5) {
-            console.log('Password should be at least 6 characters and match each other');
+            dispatch(setError('name is required'));
             return false;
-        } 
+        } else if (!validator.isEmail(email)) {
+            dispatch(setError('Email is not valid'));
+            return false;
+        } else if (password !== password2 || password.length < 5) {
+            dispatch(setError('Password should be at least 6 characters and match each other'));
+            return false;
+        }
+
+        dispatch(removeError());
 
         return true;
 
