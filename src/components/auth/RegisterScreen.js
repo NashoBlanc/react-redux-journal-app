23d8117f5380e49e41ae3ef3a258babef603b3d8
ignacioblanc <iiblanc@falabella.cl>
2021-06-11 16:05:25 -0400

    useSelector example and added msgError into screen when form is invalid

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useForm } from '../../hooks/useForm';
 import validator from 'validator';
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setError, removeError } from '../../actions/ui';
 export const RegisterScreen = () => {
 
     const dispatch = useDispatch();
-
+    const { msgError } = useSelector(state => state.ui);
 
     const [formValues, handleInputChange] = useForm({
         name: 'nashote',
@@ -49,9 +49,13 @@ export const RegisterScreen = () => {
 
             <form onSubmit={handleRegister}>
 
-                <div className="auth__alert-error">
-                        Hola Mundo
-                </div>
+              { 
+                msgError &&
+                (<div className="auth__alert-error">
+                       {
+                           msgError
+                       }
+                </div>)}
 
                 <input 
                     type="text"
