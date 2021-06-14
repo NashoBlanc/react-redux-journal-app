a3639510ba35184a079ae4fe9e6bcc2373b9d74d
ignacioblanc <iiblanc@falabella.cl>
2021-06-14 11:12:24 -0400

    keep user authenticated

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
@@ -8,8 +8,23 @@ import {
 
 import { AuthRouter } from './AuthRouter';
 import { JournalScreen } from '../components/journal/JournalScreen';
+import { firebase } from '../firebase/firebase-config';
+import { login } from '../actions/auth';
+import { useDispatch } from 'react-redux';
 
 export const AppRouter = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged((user) => {
+            if(user?.uid) {
+                dispatch(login(user.uid, user.displayName))
+            }
+        });
+    }, [dispatch])
+
+
     return (
         <Router>
             <div>
