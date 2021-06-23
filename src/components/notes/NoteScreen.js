4c9e02884f1379ccac20f47c61ce2844c4d44f00
ignacioblanc <iiblanc@falabella.cl>
2021-06-23 12:30:06 -0400

    update current note

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useRef } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { activeNote } from '../../actions/notes';
 import { useForm } from '../../hooks/useForm';
 import { NotesAppBar } from './NotesAppBar'
 
 export const NoteScreen = () => {
+
+    const dispatch = useDispatch();
 
     const {active: note} = useSelector(state => state.notes);
     const [formValues, handleInputChange, reset ] = useForm(note);
@@ -20,6 +23,12 @@ export const NoteScreen = () => {
         }
         
     }, [note, reset])
+
+
+    useEffect(() => {
+        dispatch(activeNote(formValues.id, {...formValues}));
+    }, [formValues, dispatch])
+
     return (
         <div className="notes__main-content">
             
@@ -34,6 +43,7 @@ export const NoteScreen = () => {
                     autoComplete="off"
                     value={title}
                     onChange={handleInputChange}
+                    name="title"
                 />
 
                 <textarea
@@ -41,7 +51,7 @@ export const NoteScreen = () => {
                     className="notes__textarea"
                     value={body}
                     onChange={handleInputChange}
-
+                    name="body"
                 ></textarea>
 
            {     
